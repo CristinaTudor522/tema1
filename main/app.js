165edf18c067fecda749b59abfa875af6a9de74a
CristinaTudor522 <tudorcristina18@stud.ase.ro>
2020-10-28 21:32:01 +0200

    Rezolvat

--- main/app.js
+++ main/app.js
@@ -1,5 +1,33 @@
-function removeOrderItem(orderInfo, position){
-    
+function removeOrderItem (orderInfo, position) {
+    if (orderInfo != null) {
+        //daca items este de tip vector
+        if (Array.isArray(orderInfo.items)) {
+            //verific daca fiecare obiect are atat propritatea price cat si quantity
+            for (let i=0; i< orderInfo.items.length ; i++) {
+                if (!orderInfo.items[i].hasOwnProperty('price') 
+                        || !orderInfo.items[i].hasOwnProperty('quantity')) {
+                    //altfel afisez un mesaj corespunzator
+                    throw 'Malformed item';
+                }
+            }
+            //verific daca pozitia este in legatura cu vectorul de itemi
+            if (position >= 0 && position < orderInfo.items.length) {
+                orderInfo.total = orderInfo.total 
+                        - orderInfo.items[position].price * orderInfo.items[position].quantity;
+                
+                orderInfo.items.splice(position, 1);
+
+            } else {
+                //in caz contrar arunc un mesaj corespunzator
+                throw 'Invalid position'
+            }
+
+        } else {
+            //daca items nu este vector, arunc o eroare care imi evidentiaza acest lucru
+            throw 'Items should be an array';
+        }
+    }
+    return orderInfo;
 }
 
 const app = {
